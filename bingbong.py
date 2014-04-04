90804cc1daa74620c9005096db4cf9ead7761a55
NebulaM87 <nebulam87@gmail.com>
2014-04-04 15:10:50 +0200

    Version 1.1

--- bingbong.py
+++ bingbong.py
@@ -1,63 +1,125 @@
 #!/usr/bin/python
 
 # Author: nebulam87 (nebulam87@linuxmail.org) 
-# Version: 1.0.1 
+# Version: 1.0 
 
-import urllib
 import urllib2
 import json
 import sys
 import re
- 
-def main():
-	if len(sys.argv)==2:
-		if str(sys.argv[1]) == "-h" or str(sys.argv[1]) == "--help":
-			print "Usage: " + str(sys.argv[0]) + " [your search]\n\n   If your search string contains spaces, quotes need to be added."
-			print "   Example: " + str(sys.argv[0]) + " \'cute cats\'\n"
-			exit(0)
-		else:
-			searchQuery = sys.argv[1]
-			bingResults = bing_search(searchQuery)
-			for line in bingResults:
-				line=str(line)
-				urls=re.findall(r'(https?://[^\s\'"<>]+)', line)
-		
-# Search and group as a raw string anything that starts as http(s?):// and followed by anything that is not a whitespace (\s) nor a quote or double-quote. It searches it on the string contained in variable "line"
+import optparse
 
-				if urls:
-					for i in urls:
-						if not "datamarket" in i:
-							print i	
+def bing_search(query, topValue=50):
+        #search type: Web
+        bingkey = '[insert Bing API key here]'
+        query = urllib2.quote(query)
+        # create request for authentication
+        userAgent = 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; FDM; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 1.1.4322)'
+        credentials = (':%s' % bingkey).encode('base64')[:-1]
+        auth = 'Basic %s' % credentials
+        url = 'https://api.datamarket.azure.com/Bing/Search/v1/Web?Query=%27'+query+'%27&$top='+str(topValue)+'&$format=json'
+	print "[>] Sending request with URL address: " + url + "\n"
+	request = urllib2.Request(url)
+        request.add_header('Authorization', auth)
+        request.add_header('User-Agent', userAgent)
+        request_opener = urllib2.build_opener()
+        try:
+                response = request_opener.open(request)
+        except Exception, e:
+                print "[!] Error: " + str(e) + "\n"
+                exit(0)
+        responseData = response.read()
+        jsonRes = json.loads(responseData)
+        resultList = jsonRes['d']['results']
+        return resultList
+
+
+def main():
+# Help menu and option parsing
+
+	print "##################################################"
+	print "##################################################"
+	print "##                                              ##"
+	print "##  |    _)             |                       ##"
+	print "##  __ \  | __ \   _` | __ \   _ \  __ \   _` | ##"
+	print "##  |   | | |   | (   | |   | (   | |   | (   | ##"
+	print "## _.__/ _|_|  _|\__, |_.__/ \___/ _|  _|\__, | ##"
+	print "##               |___/                   |___/  ##"
+	print "##                                              ##"
+	print "##################################################"
+	print "##################################################"
+	print "  *                                               ***********"
+	print "  * Version 1.1                                             *"
+	print "  * For bug report and suggestions: nebulam87@linuxmail.org *"
+	print "  * Please check and download upgrades from:                *"
+	print "  *        https://github.com/nebulam87/bingbong            *"
+	print "  *                                                         *"
+	print "  ***********************************************************"
+
+	parser = optparse.OptionParser()
+
+	parser.add_option('-q', '--query', dest='squery', type='string', metavar='<search query>', help='Specify the search string to send to Bing')
+	parser.add_option('-f', '--file', dest='openfile', type='string', metavar='<file name>', help='Specify the file from which to retrieve the search queries')
+	parser.add_option('-n', '--results', dest='resultNum', metavar='<number of results>', help='Specify the number of results to be retrieved')
+
+	(options, args) = parser.parse_args()
+	
+# Check if search query or file name were given
+
+	if (options.squery == None) and (options.openfile == None):
+		print parser.print_help()
+		exit(0)
 
 	else:
-		print "Usage: " + str(sys.argv[0]) + " [your search]\n\n   If your search string contains spaces, quotes need to be added."
-		print "   Example: " + str(sys.argv[0]) + " \'cute cats\'\n"
-		exit(0)
+		# If option -q given
+		if options.openfile == None:
+			if options.resultNum != None:
+				topResults=options.resultNum
+				
+			searchQuery = options.squery
+			if options.resultNum != None:
+				bingResults = bing_search(searchQuery,topResults)
+			else:
+				bingResults = bing_search(searchQuery)
+			
+			for line in bingResults:
+                                line=str(line)
+                                urls=re.findall(r'(https?://[^\s\'"<>]+)', line)
+                                if urls:
+                                        for i in urls:
+                                                if not "datamarket" in i:
+							print i
+			print "\n"
 
-def bing_search(query):
-	#search type: Web
-	bingkey = '[insert Bing API key here]'
-	query = urllib.quote(query)
-	# create request for authentication
-	userAgent = 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; FDM; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 1.1.4322)'
-	credentials = (':%s' % bingkey).encode('base64')[:-1]
-	auth = 'Basic %s' % credentials
-	url = 'https://api.datamarket.azure.com/Bing/Search/v1/Web?Query=%27'+query+'%27&$top=200&$format=json'
-	request = urllib2.Request(url)
-	request.add_header('Authorization', auth)
-	request.add_header('User-Agent', userAgent)
-	request_opener = urllib2.build_opener()
-	try:
-		response = request_opener.open(request)
-	except Exception, e:
-		print "[!] Error: " + str(e) + "\n"
-		exit(0)
-	responseData = response.read()
-	jsonRes = json.loads(responseData)
-	resultList = jsonRes['d']['results']
-	return resultList
+		# If option -f given
+		elif options.squery == None:
+			if options.resultNum != None:
+				topResults=options.resultNum
+				
+			queryFile = options.openfile
+			try:
+				filevar = open(queryFile, 'r')
+				for line in filevar.readlines():
+					searchQuery = line.strip('\n')
+                        		if options.resultNum != None:
+						bingResults = bing_search(searchQuery,topResults)
+                        		else:
+						bingResults = bing_search(searchQuery)
+					for z in bingResults:
+                                		z=str(z)
+                                		urls=re.findall(r'(https?://[^\s\'"<>]+)', z)
+                                		if urls:
+                                        		for i in urls:
+                                                		if not "datamarket" in i:
+									print i
+					print "\n"
 
- 
+
+			except Exception, e:
+				print "[-] Error " + queryFile + ": " + str(e)
+				exit(0)
+
+
 if __name__ == "__main__":
     main()
 
