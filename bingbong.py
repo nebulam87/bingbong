6bcb97d669bec1d161be698fc4971b05a53a09c5
NebulaM87 <nebulam87@gmail.com>
2014-04-24 18:18:40 +0200

    Update: Version 1.2.0

--- bingbong.py
+++ bingbong.py
@@ -9,16 +9,16 @@ import sys
 import re
 import optparse
 
-def bing_search(query, topValue=50):
-        #search type: Web
-        bingkey = '[insert Bing API key here]'
+def bing_search(query, topValue, skipValue):
+	#search type: Web
+        bingkey = '[INSERT BING API KEY HERE]'
         query = urllib2.quote(query)
         # create request for authentication
         userAgent = 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; FDM; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 1.1.4322)'
         credentials = (':%s' % bingkey).encode('base64')[:-1]
         auth = 'Basic %s' % credentials
-        url = 'https://api.datamarket.azure.com/Bing/Search/v1/Web?Query=%27'+query+'%27&$top='+str(topValue)+'&$format=json'
-	print "[>] Sending request with URL address: " + url + "\n"
+        url = 'https://api.datamarket.azure.com/Bing/Search/v1/Web?Query=%27'+query+'%27&$top='+str(topValue)+'&$format=json&$skip=' + str(skipValue)
+#	print "[>] Sending request with URL address: " + url + "\n"
 	request = urllib2.Request(url)
         request.add_header('Authorization', auth)
         request.add_header('User-Agent', userAgent)
@@ -49,7 +49,7 @@ def main():
 	print "##################################################"
 	print "##################################################"
 	print "  *                                               ***********"
-	print "  * Version 1.1                                             *"
+	print "  * Version 1.2                                             *"
 	print "  * For bug report and suggestions: nebulam87@linuxmail.org *"
 	print "  * Please check and download upgrades from:                *"
 	print "  *        https://github.com/nebulam87/bingbong            *"
@@ -60,7 +60,6 @@ def main():
 
 	parser.add_option('-q', '--query', dest='squery', type='string', metavar='<search query>', help='Specify the search string to send to Bing')
 	parser.add_option('-f', '--file', dest='openfile', type='string', metavar='<file name>', help='Specify the file from which to retrieve the search queries')
-	parser.add_option('-n', '--results', dest='resultNum', metavar='<number of results>', help='Specify the number of results to be retrieved')
 
 	(options, args) = parser.parse_args()
 	
@@ -71,54 +70,66 @@ def main():
 		exit(0)
 
 	else:
+		# Initialize the array that will contain URL addresses
+		urlList = []		
+ 
+		# Initialize skip and top values
+		skipVal = 0
+		topResults = 50
+
+                # Print init message
+                print "Query sent: " + "https://api.datamarket.azure.com/Bing/Search/v1/Web?Query=%27"+options.squery+"%27&$top="+str(topResults)+"&$format=json&$skip=" + str(skipVal)
+                print "   Searching...\n"
+
 		# If option -q given
 		if options.openfile == None:
-			if options.resultNum != None:
-				topResults=options.resultNum
-				
 			searchQuery = options.squery
-			if options.resultNum != None:
-				bingResults = bing_search(searchQuery,topResults)
-			else:
-				bingResults = bing_search(searchQuery)
-			
-			for line in bingResults:
-                                line=str(line)
-                                urls=re.findall(r'(https?://[^\s\'"<>]+)', line)
-                                if urls:
-                                        for i in urls:
-                                                if not "datamarket" in i:
-							print i
-			print "\n"
+			while skipVal < 1000:
+				bingResults = bing_search(searchQuery,topResults,skipVal)
+				for line in bingResults:
+                                	line=str(line)
+                                	urls=re.findall(r'(https?://[^\s\'"<>]+)', line)
+                                	if urls:
+                                        	for i in urls:
+                                                	if not "datamarket" in i:
+								if not i in urlList:
+									urlList.append(i)
+				skipVal = skipVal + topResults
 
 		# If option -f given
 		elif options.squery == None:
-			if options.resultNum != None:
-				topResults=options.resultNum
-				
 			queryFile = options.openfile
 			try:
 				filevar = open(queryFile, 'r')
 				for line in filevar.readlines():
 					searchQuery = line.strip('\n')
-                        		if options.resultNum != None:
-						bingResults = bing_search(searchQuery,topResults)
-                        		else:
-						bingResults = bing_search(searchQuery)
-					for z in bingResults:
-                                		z=str(z)
-                                		urls=re.findall(r'(https?://[^\s\'"<>]+)', z)
-                                		if urls:
-                                        		for i in urls:
-                                                		if not "datamarket" in i:
-									print i
-					print "\n"
+					while skipVal < 1000:
+						bingResults = bing_search(searchQuery,topResults,skipVal)
+						for z in bingResults:
+                                			z=str(z)
+                                			urls=re.findall(r'(https?://[^\s\'"<>]+)', z)
+                                			if urls:
+                                        			for i in urls:
+                                                			if not "datamarket" in i:
+										if not i in urlList:
+											urlList.append(i)
+	
+					skipVal = skipVal + topResults
 
 
 			except Exception, e:
 				print "[-] Error " + queryFile + ": " + str(e)
 				exit(0)
-
+		
+		# Print the URL list.
+		if urlList:
+			numUrls = len(urlList)
+			print "[+] " + str(numUrls) + " results found:\n"
+			for urlAddresses in urlList:				
+				print urlAddresses
+			print "\n"
+		else:
+			print "[-] No results.\n"
 
 if __name__ == "__main__":
     main()
